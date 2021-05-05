220611668dcf2723ef75ac9f186a9a50df14b691
Vitor Paholsky <vitor_paduarte@hotmail.com>
2021-05-04 23:00:49 -0300

    frontend 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,35 @@
+import BarChart from "components/BarChart"
+import NavBar from "components/NavBar"
+import Footer from "components/Footer"
+import DataTable from "components/DataTable"
+import DonutChart from "components/DonutChart"
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello World!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>   
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas vendas</h5>
+            <DonutChart />
+          </div>   
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
